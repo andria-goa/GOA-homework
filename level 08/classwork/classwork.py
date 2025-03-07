0e463cd0448bbd351b9a0651fce50aba4590b333
andria-goa <androandria6@gmail.com>
2025-03-07 23:50:50 +0400

    commit

---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,30 @@
+# 1) type ნახავს თუ რომელ დათა ბაზაშია ის სიტყვა ან რაც გიწერია
+
+# 2)
+name = 'andria'
+age = 12
+idk_random_number = 9.1
+random1 = False
+random2 = True
+
+
+print(type(name))
+print(type(age))
+print(type(idk_random_number))
+print(type(random1))
+print(type(random2))
+
+# 3)
+x = 20
+y = 10
+
+plus = x + y
+minus = x - y
+devision = x / y
+equasion = x * y
+
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
---
+++ level 08/homework/homework.py
@@ -0,0 +1,20 @@
+
+# დავალება 3.
+#str = "hello"
+#int --- 5
+#float --- 6.8
+#bool --- False
+
+# დავალება 4.
+num1 = 19
+num2 = 59
+print(num1 + num2)
+#რადგან ინტეგერს სტრინგს ვერ მივუმატებთ
+
+# დავალება 5.
+first = "hello, my name is andria patsatsia. "
+second = "i live in georgia. "
+third =  "you should check out georgia it is nice country, "
+forth = "our popular food is kinkali, its very yummy."
+
+print(first + second + third + forth)
