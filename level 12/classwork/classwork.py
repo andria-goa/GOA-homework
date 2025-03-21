1bab25137fbf23c5fd66cb50bb57728269562fc7
andria-goa <androandria6@gmail.com>
2025-03-21 22:16:12 +0400

    commit

---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,44 @@
+
+one = 10 != 10
+two = 10 == 10
+
+print(one)
+print(two)
+
+# false გამოიტანს მთლიანობაში
+
+
+
+
+
+#other classwork
+
+# print(False or True and False or True)
+#True 
+
+# print(False and True or True and False or True)
+#True
+
+# print((True and False) or (False or True and True))
+#True
+
+# print(True and True or False and False or True)
+#True
+
+# print((False or True) and (True or False and True))
+#True
+
+#print(False and True or False or False and True)
+#false
+
+#print(True and False or False and True or False)
+#false
+
+#print((False or False) and (True and False))
+#false
+
+#print(True and False and True or False)
+#false
+
+#print((False or False) and (False or True and False))
+#false
---
+++ level 12/homework/homework1.py
@@ -0,0 +1,13 @@
+
+# 1)
+print(5 < 6)
+
+print()
+
+# 2) 
+print(True or False and 5 != 6)
+
+print()
+
+# 3) 
+print(False and False and True)
