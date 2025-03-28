288ec7657badc88533a80e7e1d075ba6cb620481
andria-goa <androandria6@gmail.com>
2025-03-28 22:48:50 +0400

    commit

---
+++ level 14/classwork/index.py
@@ -0,0 +1,10 @@
+
+# კომენტარის სახით დამიწერეთ რა არის(sequence,selection,iteration,algorith)
+
+#sequence არის თანმიმდევრობა
+
+#selection არის არჩევა
+
+#iteration არის გამეორება
+
+#algorith არის ინსტრუქცია რომელსაც თანმიმდევრობით უნდა მიყვე
