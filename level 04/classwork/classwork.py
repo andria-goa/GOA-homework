5c65366c50c9ee17517005d3c4508ad6ce3b2549
andria-goa <androandria6@gmail.com>
2025-04-04 22:49:21 +0400

    commit

--- level 04/classwork/classwork.py
+++ level 04/classwork/classwork.py
@@ -1,18 +1,18 @@
 #our classwork was to make mistakes and say what were the mistakes
 
-print('andria")
+# print('andria")
 #სხვადასხვა ფრჩხილებია როცა ერთიდაიგივე უნდა იყოს
 
-print("5" + 5)
+# print("5" + 5)
 #არ შეიძლება სიტყვას მიუმატო რიცხვი
 
-print("world)
+# print("world)
 #აკლია ერთი ფრჩხილი
 
-print("class XII"
+# print("class XII"
 #აკლია ) ნიშანი
 
-fruit = apple
+# fruit = apple
 #მე მინდა ამოვბეჭდო apple
-print("fruit")
+# print("fruit")
 #შეცდომა ის არის რომ სიტყვა fruit ამოვბეჭდე და არა ცვლადი
---
+++ level 16/classwork/index.py
@@ -0,0 +1,23 @@
+
+# 1) მომხმარებელს შემოატანინე სახელი, გვარი, ასაკი, 
+# და შემდეგ დაპრინტე
+
+# ასევე  მომხმარებელს შემოატანინე 2 ეიცხვი, და ერთმანეთს მიუმატე გამოაკელი გაამრავლე და გაყავი.
+
+name = input()
+surname = input()
+age = input()
+
+print(name)
+print(surname)
+print(age)
+
+
+
+num1 =  input()
+num2 =  input()
+
+print(int(num1 + num2))
+print(int(num1 - num2))
+print(int(num1 / num2))
+print(int(num1 * num2))
